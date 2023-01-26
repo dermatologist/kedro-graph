88bd4a4ea35113fe42d9771785e5e6ae1dab8151
Bell Eapen <github_public@gulfdoctor.net>
2023-01-26 20:38:38 +0000

    embedding in progress

--- src/kedro_graph/pipeline_registry.py
+++ src/kedro_graph/pipeline_registry.py
@@ -5,6 +5,7 @@ from kedro.framework.project import find_pipelines
 from kedro.pipeline import Pipeline
 
 from kedro_tf_utils.pipelines.embedding.pipeline import create_embedding_pipeline
+from kedro_tf_text.pipelines.tabular.pipeline import create_tabular_model_pipeline
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -14,5 +15,5 @@ def register_pipelines() -> Dict[str, Pipeline]:
     """
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
-    pipelines["embedding"] = create_embedding_pipeline()
+    pipelines["embedding"] = create_tabular_model_pipeline() + create_embedding_pipeline()
     return pipelines
