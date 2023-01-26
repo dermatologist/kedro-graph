98946c9bce7379d205f6d26a49be9753cee7be7c
Bell Eapen <github_public@gulfdoctor.net>
2023-01-26 22:03:30 +0000

    Embeddings working

--- src/kedro_graph/pipeline_registry.py
+++ src/kedro_graph/pipeline_registry.py
@@ -6,6 +6,7 @@ from kedro.pipeline import Pipeline
 
 from kedro_tf_utils.pipelines.embedding.pipeline import create_embedding_pipeline
 from kedro_tf_text.pipelines.tabular.pipeline import create_tabular_model_pipeline
+from kedro.pipeline.modular_pipeline import pipeline as modular_pipeline
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -13,7 +14,12 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from pipeline names to ``Pipeline`` objects.
     """
+    tabular_inputs = {"parameters": "params:embedding", "model": "tabular_model",
+              "tabular_data": "tabular_data", "outputs": "tabular_embedding"}
+    image_inputs = {"parameters": "params:embedding", "model": "image_model",
+                      "image_data": "image_data", "outputs": "image_embedding"}
     pipelines = find_pipelines()
     pipelines["__default__"] = sum(pipelines.values())
-    pipelines["embedding"] = create_tabular_model_pipeline() + create_embedding_pipeline()
+    pipelines["tabular"] = create_tabular_model_pipeline() + create_embedding_pipeline(**tabular_inputs)
+    pipelines["image"] = create_embedding_pipeline(**image_inputs)
     return pipelines
